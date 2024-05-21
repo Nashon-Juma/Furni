14fa60f5a64c64a60289d75fb33ef7bf3fdf7c65
Nashon Juma <Nahshonjumaz@gmail.com>
2024-05-21 10:22:13 +0300

    models

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Image extends Model
 {
     use HasFactory;
+    protected $fillable = ['product_id', 'path'];
+    public function product()
+    {
+        return $this->hasOne(Product::class, 'main_image_id');
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+namespace App\Models;
+use Money\Money;
+use Money\Currency;
+use App\Models\Image;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasAttributes;
 
 class Product extends Model
 {
+    // use HasAttributes;
+
     use HasFactory;
+    public function mainImage()
+    {
+        return $this->belongsTo(Image::class, 'main_image_id');
+    }
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn (int $value) => new Money($value, new Currency('USD')),
+        );
+    }
 }
